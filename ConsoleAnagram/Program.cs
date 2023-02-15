f0e087845368204ab7ec59cb61f5f4494d5df70d
Denys <navrotskyidenys@gmail.com>
2023-02-15 09:12:38 +0200

    Add new Projects

---
+++ ConsoleAnagram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAnagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
