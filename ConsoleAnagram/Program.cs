1e514e115d616351fd0cfb7c3bf4a9dc4331cbc4
jede34 <thefrapspro@gmail.com>
2023-02-16 13:58:10 +0200

    add new files

---
+++ ConsoleAnagram/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleAnagram;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ LibraryAnagramTest/AnagramTest.cs
@@ -0,0 +1,16 @@
+namespace LibraryAnagramTest
+{
+    public class AnagramTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
