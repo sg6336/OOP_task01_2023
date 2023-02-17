043b826d12d6aa433bcaca6370e09c284ab60c5d
Oganes <maxogo1234@gmail.com>
2023-02-17 11:53:47 +0200

    add new fileas

---
+++ ConsoleAnagram/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleAnagram.Resources;
+using LibraryAnagram;
+
+namespace ConsoleAnagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
