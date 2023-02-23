e55270be0774a81a81ae913debe8efcbbdf8aea9
Levsha <5656749@stud.nau.edu.ua>
2023-02-23 14:24:57 +0200

    add resourses

--- ConsoleAnagram/Program.cs
+++ ConsoleAnagram/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ConsoleAnagram
+﻿using ConsoleAnagram.Resourses;
+namespace ConsoleAnagram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(Messages.String1);
         }
     }
 }
