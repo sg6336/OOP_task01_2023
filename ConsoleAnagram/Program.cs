f9c5d26c54023b226d70201edcec8a170491beff
Oganes <maxogo1234@gmail.com>
2023-03-03 10:55:46 +0200

    res

--- ConsoleAnagram/Program.cs
+++ ConsoleAnagram/Program.cs
@@ -7,7 +7,30 @@ namespace ConsoleAnagram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.CursorVisible= true;
+            Console.WriteLine(Messages.Greetings);
+            ConsoleKeyInfo Button;
+
+            do
+            {
+                var obj = new Anagram();
+                Console.WriteLine();
+                Console.WriteLine(Messages.Action);
+                Console.WriteLine();
+
+                string input = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine(Messages.Result + obj.Reverse(input));
+                Console.WriteLine();
+                Console.WriteLine(Messages.Last_Action);
+
+                Button = Console.ReadKey();
+                if (Button.Key == ConsoleKey.Spacebar)
+                {
+                    Environment.Exit(0);
+                }
+            } while (Button.Key == ConsoleKey.Enter);
         }
     }
-}
+}   
--- LibraryAnagramTest/AnagramTest.cs
+++ LibraryAnagramTest/AnagramTest.cs
@@ -4,15 +4,37 @@ namespace LibraryAnagramTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        Anagram objtest = new Anagram();
+
+        [Test]
+        [TestCase("Test", "tseT")]
+        [TestCase("agog", "goga")]
+        [TestCase("a!bcd efg5h", "d!cba hgf5e")]
+        [TestCase("kap!! (0)pus", "pak!! (0)sup")]
+
+        public void IsQuickTest(string testString, string expectedResult)
         {
+            Assert.That(objtest.Reverse(testString), Is.EqualTo(expectedResult));
         }
 
         [Test]
-        public void Test1()
+        [TestCase("nba", "abn")]
+        [TestCase("holo", "oloh")]
+        [TestCase("hiphop", "pohpih")]
+        [TestCase("n*ba", "a*bn")]
+        [TestCase("l*bby", "y*bbl")]
+        [TestCase("momm !! y */hug21e", "mmom !! y */egu21h")]
+        public void IsMixTest(string testString, string expectedResult)
         {
-            Assert.Pass();
+            Assert.That(objtest.Reverse(testString), Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(null, "")]
+        [TestCase("", "")]
+        public void IsNullOrEmptyTest(string testString, string expectedResult)
+        {
+            Assert.That(objtest.Reverse(testString), Is.EqualTo(expectedResult));
         }
     }
 }
