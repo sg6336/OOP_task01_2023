1c23882b139b6873f3309e700ec3712110b941c6
Tiomych <tiomych.love@gmail.com>
2023-05-23 20:53:06 +0300

    modified

--- LibraryAnagram/Anagram.cs
+++ LibraryAnagram/Anagram.cs
@@ -1,7 +1,9 @@
-﻿namespace LibraryAnagram
-{
-    public class Anagram
-    {
+﻿using System;
 
+public static class Kata
+{
+    public static int DiagonalSum(int[,] matrix)
+    {
+        //TODO
     }
 }
--- LibraryAnagramTest/AnagramTest.cs
+++ LibraryAnagramTest/AnagramTest.cs
@@ -1,16 +1,48 @@
+using LibraryAnagram;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System.Globalization;
+using System;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Constraints;
+
 namespace LibraryAnagramTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        Anagram objtest = new Anagram();
+
+        [Test]
+        [TestCase("abcd", "dcba")]
+        [TestCase("qwer1t", "trew1q")]
+        [TestCase("a!bcd efg*h", "d!cba hgf*e")]
+        [TestCase("tyr rew", "ryt wer")]
+
+        public void IsQuickTest(string testString, string expectedResult)
         {
+            Assert.That(objtest.Reverse(testString), Is.EqualTo(expectedResult));
         }
 
         [Test]
-        public void Test1()
+        [TestCase("Test", "tseT")]
+        [TestCase("Hello!", "olleh!")]
+        [TestCase("yes. Yes", "sey. seY")]
+        [TestCase("1 + 2 = 3", "1 + 2 = 3")]
+        [TestCase(" Anagram6", " marganA6")]
+
+        public void IsMixTest(string testString, string expectedResult)
         {
-            Assert.Pass();
+            Assert.That(objtest.Reverse(testString), Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase("", "")]
+        [TestCase(null, "")]
+
+        public void IsNullOrEmptyTest(string testString, string expectedResult)
+        {
+            Assert.That(objtest.Reverse(testString), Is.EqualTo(expectedResult));
         }
     }
 }
